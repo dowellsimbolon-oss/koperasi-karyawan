5bd5cd640bc5c13a715a2e03cbca9a2192996dad
dowellsimbolon-oss <dowellsimbolon@gmail.com>
2026-09-24 14:15:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Konfigurasi URL Base REST API Back-End Node.js
-const API_BASE_URL = 'https://koperasi-karyawan-ivia.vercel.app';
+const API_BASE_URL = 'https://koperasi-backend-g1z7u7hh2-frans-dowell.vercel.app/';
 
 let chartInstance = null;
 
