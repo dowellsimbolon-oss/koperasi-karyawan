db3b5f1dc98f9fcaa21ee59cd5f4fb05c3c6bb65
dowellsimbolon-oss <dowellsimbolon@gmail.com>
2026-09-24 12:09:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function handleLogin(event) {
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch(`${API_BASE_URL}/auth/login`, {
+       fetch(`${API_BASE_URL}/api/auth/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nik, password })
